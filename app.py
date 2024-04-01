c2d36e50e76adb24d2106644ee89e120230cc6a5
MilenaMakarova <m.makarova75@gmail.com>
2024-04-01 22:43:29 +0900

    Add a form to sum two numbers

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index_page():
-    return render_template("index.html")
+     try:
+        a = int(request.args["a"])
+        b = int(request.args["b"])
+        summa = a + b
+     except:
+        a, b = 0, 0
+        summa = "Не удалось вычислить сумму"
+     return render_template("index.html", answer=summa, a=a, b=b)
