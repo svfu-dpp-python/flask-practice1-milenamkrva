c1583f0cd71c67dc7a391bf9e796a984cae331c7
MilenaMakarova <m.makarova75@gmail.com>
2024-04-01 23:00:57 +0900

    Change the form method to POST

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index_page():
-     try:
-        a = int(request.args["a"])
-        b = int(request.args["b"])
+    try:
+        a = int(request.form["a"])
+        b = int(request.form["b"])
         summa = a + b
-     except:
+    except:
         a, b = 0, 0
         summa = "Не удалось вычислить сумму"
-     return render_template("index.html", answer=summa, a=a, b=b)
+    return render_template("index.html", answer=summa, a=a, b=b)
